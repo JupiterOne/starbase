b66c738e712376b64aec67e80c99c960273b0434
Adam Pierson <adam.pierson@jupiterone.com>
2022-03-07 16:24:13 -0600

    Making integration instance ID a required option for wipe command.

--- src/cli/wipe.ts
+++ src/cli/wipe.ts
@@ -3,11 +3,10 @@ import { executeWithLogging } from '../starbase/process';
 
 export function wipe() {
   return createCommand('wipe')
-    .description('wipes data from Neo4j instance')
-    .option(
+    .description('wipes data from Neo4j instance with specified instance ID')
+    .requiredOption(
       '-i, --integration-instance-id <id>',
-      '_integrationInstanceId assigned to uploaded entities',
-      'defaultLocalInstanceID',
+      '_integrationInstanceId assigned to uploaded entities that should be wiped from Neo4j',
     )
     .action(async (options) => {
       executeWithLogging(`yarn j1-integration neo4j wipe -i ${options.integrationInstanceId}`);
