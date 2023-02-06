38e7d4d5593c8cdd36753ac7cc33606393c99ecf
Adam Pierson <adam.pierson@jupiterone.com>
2023-02-06 08:08:38 -0500

    Explicitly filtering out optional config fields before schema check.

--- src/starbase/setup.ts
+++ src/starbase/setup.ts
@@ -47,7 +47,11 @@ async function checkInstanceConfigFields(
         // of our integrations.
         const capsConfig = Object.assign(
           {},
-          ...Object.keys(invocationConfig.instanceConfigFields).map((key) => ({
+          ...Object.keys(invocationConfig.instanceConfigFields).filter(function(key) {
+            // Explicitly filter out optional config fields before performing schema check
+            if(!invocationConfig.instanceConfigFields[key]?.optional)
+              return key
+          }).map((key) => ({
             [snakecase(key).toUpperCase()]:
               invocationConfig.instanceConfigFields[key],
           })),
